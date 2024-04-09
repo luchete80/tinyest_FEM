1d393297b28bc4b3e872e03a33434e6ebce3e905
luciano nb <luciano.buglioni@gmail.com>
2024-04-09 18:34:58 -0300

    added full int examples

--- 1el_3D_red_int.py
+++ 1el_3D_red_int.py
@@ -1,25 +1,24 @@
 import numpy as np
+
 # Define material properties
+
 E = 200e9  # Young's modulus in Pa
 nu = 0.3   # Poisson's ratio
-m_dim = 2
-m_nodxelem = 4
+m_dim = 3
+m_nodxelem = 8
 # Define element properties
 
 element_length = 1.0   # Length of the element
 m_gp_count = 1
 
-dNdX = np.zeros((m_gp_count, m_dim, m_nodxelem)) 
-dNdrs = np.zeros((m_gp_count, m_dim, m_nodxelem)) 
+dNdX = np.zeros((m_dim, m_nodxelem)) 
+dNdrs = np.zeros((m_dim, m_nodxelem)) 
 # Define shape functions and their derivatives for 2D quadrilateral element
 def shape_functions(xi, eta):
     dNdX_ = np.zeros((m_dim, m_nodxelem))
-    N = np.array([(1-xi)*(1-eta)/4,
-                  (1+xi)*(1-eta)/4,
-                  (1+xi)*(1+eta)/4,
-                  (1-xi)*(1+eta)/4])
-    dNdX_[0,:] = np.array([-(1-eta)/4, (1-eta)/4, (1+eta)/4, -(1+eta)/4])
-    dNdX_[1,:] = np.array([-(1-xi)/4, -(1+xi)/4, (1+xi)/4, (1-xi)/4])
+    N = np.array([0.125,0.125,0.125,0.125,0.125,0.125,0.125,0.125])
+    # dNdX_[0,:] = np.array([-(1-eta)/4, (1-eta)/4, (1+eta)/4, -(1+eta)/4])
+    # dNdX_[1,:] = np.array([-(1-xi)/4, -(1+xi)/4, (1+xi)/4, (1-xi)/4])
     return N, dNdX_
     print(dNdX)
 # Gauss quadrature points and weights
@@ -33,19 +32,32 @@ gauss_weights = np.array([1, 1, 1, 1])
 gp_count = len(gauss_points)
 
 # Finite element strain rate calculation
-def calculate_jacobian(pos):
-    J = np.zeros((gp_count, 2, 2))
-    for gp in range(len(gauss_points)):
-        xi, eta = gauss_points[gp]
-        weight = gauss_weights[gp]
-        N, dNdrs[gp] = shape_functions(xi, eta)
-        J[gp] = np.dot(dNdrs[gp], pos)
-        detJ = np.linalg.det(J[gp])
-        print("det J\n", detJ)
-        invJ = np.linalg.inv(J[gp])
-        print ("invJ", invJ)
-        dNdX[gp] = np.dot(invJ,dNdrs[gp])
-    return J
+def calculate_jacobian(x2):
+    J = np.zeros((m_dim, m_dim))
+    # x2 = np.zeros((m_nodxelem,m_dim))
+    # x2(i,:)=nod%x(x[:,i]
+    # N, dNdrs[gp] = shap1_functions(xi, eta)
+    #J[gp] = np.dot(dNdrs1gp], pos)
+    print ("x2")
+    print(x2)
+    J[0,:] = -x2[0,:]+x2[1,:]+x2[2,:]-x2[3,:]-x2[4,:]+x2[5,:]+x2[6,:]-x2[7,:]
+    J[1,:] = -x2[0,:]-x2[1,:]+x2[2,:]+x2[3,:]-x2[4,:]-x2[5,:]+x2[6,:]+x2[7,:]
+    J[2,:] = -x2[0,:]-x2[1,:]-x2[2,:]-x2[3,:]+x2[4,:]+x2[5,:]+x2[6,:]+x2[7,:]
+    
+    detJ = np.linalg.det(J)
+    print("det J\n", detJ)
+    invJ = np.linalg.inv(J)
+    print ("invJ", invJ)
+    dNdX[:,0] = -invJ[:,0]-invJ[:,1]-invJ[:,2] #For each 3 rows of inv J and dHdxy
+    dNdX[:,1] =  invJ[:,0]-invJ[:,1]-invJ[:,2]
+    dNdX[:,2] =  invJ[:,0]+invJ[:,1]-invJ[:,2]
+    dNdX[:,3] = -invJ[:,0]+invJ[:,1]-invJ[:,2]
+    dNdX[:,4] = -invJ[:,0]-invJ[:,1]+invJ[:,2]
+    dNdX[:,5] =  invJ[:,0]-invJ[:,1]+invJ[:,2]
+    dNdX[:,6] =  invJ[:,0]+invJ[:,1]+invJ[:,2]
+    dNdX[:,7] = -invJ[:,0]+invJ[:,1]+invJ[:,2]
+        
+    return J,dNdX 
 
 def velocity_gradient_tensor(dNdX, vel):
     grad_v = np.zeros((m_gp_count,m_dim, m_dim))
@@ -54,7 +66,7 @@ def velocity_gradient_tensor(dNdX, vel):
             for J in range(m_dim):
                 for k in range(m_nodxelem): 
                     #grad_v[gp,I, J] += dNdX[gp, J, k] * vel[k * m_dim + I]
-                    grad_v[gp,I, J] += dNdX[gp, J, k] * vel[k, I]
+                    grad_v[gp,I, J] += dNdX[J, k] * vel[k, I]
     return grad_v
 
 def calc_str_rate (dNdX,velocities):
@@ -101,7 +113,8 @@ def calculate_stress(eps,dNdX):
 vel = np.full(m_dim * m_nodxelem, 0.1)
 vel[5] = vel[7] = -1.0
     
-x    =  np.array([[0., 0.], [0.1, 0.], [0.1, 0.1], [0., 0.1]])
+x    =  np.array([[0., 0.,0.], [0.1, 0.,0.], [0.1, 0.1,0.0], [0., 0.1,0.0],
+                  [0., 0.,0.1], [0.1, 0.,0.1], [0.1, 0.1,0.1], [0., 0.1,0.1]])                  
 velocities = np.array([[0, 0], [0, 0], [0, -1], [0, -1]])  # Example velocities at nodes
 #strain_rate = calculate_strain_rate(velocities)
 J = calculate_jacobian(x)
