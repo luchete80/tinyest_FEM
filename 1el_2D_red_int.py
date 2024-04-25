331ac7928e6101793e41417d3a8b911b9ee6e323
luciano nb <luciano.buglioni@gmail.com>
2024-04-24 22:02:55 -0300

    added 2d

--- 1el_2D_red_int.py
+++ 1el_2D_red_int.py
@@ -10,22 +10,34 @@ m_nodxelem = 4
 element_length = 1.0   # Length of the element
 m_gp_count = 1
 
-# Define nodal velocities (dummy data for demonstration)
+# Define nodal v (dummy data for demonstration)
 vel = np.full(m_dim * m_nodxelem, 0.1)
 vel[5] = vel[7] = -1.0
+
+dt = 0.8e-5
     
-x    =  np.array([[0., 0.], [0.1, 0.], [0.1, 0.1], [0., 0.1]])
-velocities = np.array([[0, 0], [0, 0], [0, -1], [0, -1]])  # Example velocities at nodes
+x      =  np.array([[0., 0.], [0.1, 0.], [0.1, 0.1], [0., 0.1]])
+v      = np.array([[0, 0], [0, 0], [0, -1], [0, -1]])  # Example v at nodes
 
 ############################
 # Gauss quadrature points and weights
+a      = np.zeros((m_nodxelem,2)) 
+u      = np.zeros((m_nodxelem,2)) 
+u_tot  = np.zeros((m_nodxelem,2))
+prev_a = np.zeros((m_nodxelem,2)) 
+
 gauss_points = np.array([[0.0, 0.0]])
-gauss_weights = np.array([4.0])
-mass = np.zeros((m_nodxelem))
+gauss_weights = np.array([1.0])
 detJ = np.zeros((m_gp_count))
 dNdX = np.zeros((m_gp_count, m_dim, m_nodxelem)) 
 dNdrs = np.zeros((m_gp_count, m_dim, m_nodxelem)) 
 
+strain = np.zeros((m_gp_count,m_dim, m_dim))
+
+def impose_bcv(vel):
+  vel[2,1] = vel[2,1] = -1.0
+  vel[0,:] = vel[1,0] = 0.0
+  
 # Define shape functions and their derivatives for 2D quadrilateral element
 def shape_functions(xi, eta):
     dNdX_ = np.zeros((m_dim, m_nodxelem))
@@ -90,10 +102,10 @@ def velocity_gradient_tensor(dNdX, vel):
                     grad_v[gp,I, J] += dNdX[gp, J, k] * vel[k, I]
     return grad_v
 
-def calc_str_rate (dNdX,velocities):
+def calc_str_rate (dNdX,v):
     str_rate = np.zeros((m_gp_count,m_dim, m_dim))
     for gp in range (m_gp_count):
-        grad_v = velocity_gradient_tensor(dNdX, velocities)
+        grad_v = velocity_gradient_tensor(dNdX, v)
         print("Velocity gradients\n" ,grad_v[0])
 
         str_rate[gp] = 0.5*(grad_v[0]+grad_v[0].T)
@@ -102,11 +114,11 @@ def calc_str_rate (dNdX,velocities):
 
 
 
-# Define material matrix for plane stress
+# Define material matrix for plane strain
 def material_matrix():
-    C = E / (1 - nu**2) * np.array([[1, nu, 0],
-                                     [nu, 1, 0],
-                                     [0, 0, (1 - nu) / 2]])
+    C = E / ((1+nu)*(1 - 2.0*nu)) * np.array([[1-nu, nu, 0],
+                                     [nu, 1-nu, 0],
+                                     [0, 0, (1 - 2.0*nu) ]])
     return C
 
 def calc_strain(str_rate,dt):
@@ -146,23 +158,58 @@ def calc_forces(stress,dNdX,J):
     return forces
 
 
-#strain_rate = calc_strain_rate(velocities)
+#strain_rate = calc_strain_rate(v)
 J, detJ, dNdX = calc_jacobian(x)
 print ("Jacobian\n", J[0])
 print ("det J \n", detJ[0])
 vol_0 = calc_vol(detJ)
-
-dt = 8.0e-5
-str_rate = calc_str_rate (dNdX,velocities)
-
-strain =  calc_strain(str_rate,dt)
-stress =  calc_stress(strain,dt)
-forces =  calc_forces(stress,dNdX,J)
+nod_mass = vol_0 * rho / m_nodxelem 
 
 # accel ()= forces/mass
 
+rho_b = 0.8182  # DEFAULT SPECTRAL RADIUS
 
+alpha = (2.0 * rho_b - 1.0) / (1.0 + rho_b)
+beta  = (5.0 - 3.0 * rho_b) / ((1.0 + rho_b) * (1.0 + rho_b) * (2.0 - rho_b))
+gamma = 1.5 - alpha;
 
+print ("V", a)
+################################# MAIN LOOP ###################################
+for step in range(1):
+    # !!! PREDICTION PHASE
+    u = dt * (v + (0.5 - beta) * dt * prev_a)
+    # !!! CAN BE UNIFIED AT THE END OF STEP by v= (a(t+dt)+a(t))/2. but is not convenient for variable time step
+    v = v + (1.0-gamma)* dt * prev_a
+    a[:,:] = 0.0
+    impose_bcv(v)
+
+    J, detJ, dNdX = calc_jacobian(x)
+    # vol_0 = calc_vol(detJ)
+    # nod_mass = vol_0 * rho / m_nodxelem 
+
+    str_rate = calc_str_rate (dNdX,v)
+    print ("strain rate\n",str_rate)
+    # strain =  strain + calc_strain(str_rate,dt)
+    strain =  calc_strain(str_rate,dt)
+    stress =  calc_stress(strain,dt)
+    print ("stress\n",stress)
+    forces =  calc_forces(stress,dNdX,J)
+
+    a = a - alpha * prev_a
+    a = a / (1.0 - alpha)
+    v = v + gamma * dt * a  
+
+    impose_bcv (v) #REINFORCE VELOCITY BC
+
+    u = u + beta * dt * dt * a   
+    # nod%u = nod%u + u
+    x = x + u
+    
+    # !call AverageData(elem%rho(:,1),nod%rho(:))  
+    prev_a = a
+    # time = time + dt
+  
+print ("DISPLACEMENTS\n",u)
 print (strain)
 print("STRESS")
 print (stress)
